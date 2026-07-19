5ca9e0df1ebe1244286dee389087fed9da2c91cb
Audrey Carol-Grace Hester <hesterau@msu.edu>
2026-07-19 12:59:48 -0500

    Use network-first strategy in service worker so app updates aren't stuck behind stale cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'load-log-v1';
+const CACHE = 'load-log-v2';
 const SHELL = ['./', './index.html', './manifest.json', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(event){
@@ -20,12 +20,9 @@ self.addEventListener('fetch', function(event){
     return;
   }
   event.respondWith(
-    caches.match(req).then(function(cached){
-      const network = fetch(req).then(function(res){
-        if(res && res.ok) caches.open(CACHE).then(function(cache){ cache.put(req, res.clone()); });
-        return res;
-      }).catch(function(){ return cached; });
-      return cached || network;
-    })
+    fetch(req).then(function(res){
+      if(res && res.ok) caches.open(CACHE).then(function(cache){ cache.put(req, res.clone()); });
+      return res;
+    }).catch(function(){ return caches.match(req); })
   );
 });
